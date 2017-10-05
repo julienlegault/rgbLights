3abc8735972e76ba6bf06c8c692604c6a744566e
julienlegault <julien.p.legault@gmail.com>
2017-10-05 12:36:27 -0500

    It now does some things, more features to come.

--- lights.py
+++ lights.py
@@ -1,37 +1,55 @@
 from datetime import datetime as dt
 from time import sleep
+import pigpio
 
+RED_PIN = 17
+GREEN_PIN = 22
+BLUE_PIN = 24
+
+pi = pigpio.pi()
+
+def setcolor(array):
+   pi.set_PWM_dutycycle(RED_PIN, array[0])
+   pi.set_PWM_dutycycle(GREEN_PIN, array[1])
+   pi.set_PWM_dutycycle(BLUE_PIN, array[2])
 def sanitary(rgbcode):
    rgb = rgbcode[1:len(rgbcode)]
    rgbarray = rgb.split('.')
+   rgbarray = [int(x) for x in rgbarray]
+   for i in range(0,3):
+      if(rgbarray[i] < 0):
+         rgbarray[i] = 0
+      if(rgbarray[i] > 255):
+         rgbarray[i] = 255
    return rgbarray
 
 def toRGB(hexcode):
-   return ".0.100.200"
+   hexcodeReal = hexcode[1:7]
 
 def sunrise(riseTime):
-  print("rgb: 0,0,0")
-  print(dt.now().hour)
+  setColor([0,0,0])
   riseTime = int(riseTime) - 1
   if(dt.now().hour == riseTime):
       sleep(600)
-      print("rgb: 10,0,0")
+      setColor([10,0,0])
       sleep(600)
-      print("rgb: 50,0,0")
+      setColor([50,0,0])
       sleep(300)
-      print("rgb: 100,50,50")
+      setColor([100,50,50])
       sleep(300)
-      print("rgb: 255,100,100")
+      setColor([255,100,100])
 
 while(True):
    userInput = input()
    if(userInput[0] == '#'):
-      print("hex: " + str(sanitary(toRGB(userInput))))
+      setColor((sanitary(toRGB(userInput))))
    elif(userInput[0] == '.'):
-      print("rgb: " + str(sanitary(userInput)))
+      setColor(sanitary(userInput)))
    elif(userInput == "War Eagle"):
       print("WDE")
    elif(userInput[0:7] == "sunrise"):
       sunrise(userInput[7:len(userInput)])
    else:
       continue
+
+pi.stop()
